8db1b972faa0199e6b1899f37886ee8eb93fcc89
Caven Fernanda <fernanda17caven@gmail.com>
2021-06-07 23:24:27 +0200

    Initial Commit
    University and Faculty entities declared

---
+++ src/main/java/za/ac/cput/Entity/Faculty.java
@@ -0,0 +1,9 @@
+package za.ac.cput.Entity;
+
+/* Faculty.java
+   Entity for Faculty
+   Author: Caven Fernanda - 218074905
+   Date: 05 June 2021
+ */
+public class Faculty {
+}
---
+++ src/main/java/za/ac/cput/Entity/University.java
@@ -0,0 +1,9 @@
+package za.ac.cput.Entity;
+
+/* University.java
+   Entity for University
+   Author: Caven Fernanda - 218074905
+   Date: 05 June 2021
+ */
+public class University {
+}
