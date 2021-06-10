940c81be6ca6f77167d9877fc7d16d8837a0b231
Project3Assignment <mfortuin509@gmail.com>
2021-06-10 23:24:37 +0200

    added testCases for Test class

--- src/test/java/za/ac/cput/Factory/SubjectFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/SubjectFactoryTest.java
@@ -1,7 +1,12 @@
 package za.ac.cput.Factory;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.Entity.Subject;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,17 +22,54 @@ import static org.junit.jupiter.api.Assertions.*;
 class SubjectFactoryTest
 {
 
-    private Subject sub1;
-    private Subject sub2;
-    private Subject sub3;
+    private Subject.Builder sub1;
+    private Subject.Builder sub2;
+    private Subject.Builder sub3;
 
 
     @BeforeEach
  void setup()
 {
 
-    sub1 = new Subject();
+    sub1 = new Subject.Builder().subjectName("PHP");
+    sub2 = new Subject.Builder().subjectName("JAVA");
+    sub3=sub1;
 
+}
+
+//Object Equality
+    @Test
+
+     void testObjectEquality()
+    {
+        //Shallow Comparison (==)
+        assertSame(sub1,sub3);
+    }
+
+//Object Identity
+    @Test
+
+     void testObjectIdentity()
+    {
+        //deep Comparison (.equals)
+        assertNotEquals(sub1,sub2);
+    }
+
+//Timeout
+    @Test
+    @Timeout (4)
+
+    void timeoutTest()
+    {
+      while(true);
+    }
+
+//Disabling
+  @Disabled("Do not run in alpha version")
+  @Test
+   void compare()
+{
+    assertEquals(sub1,sub2);
 }
 
 }
