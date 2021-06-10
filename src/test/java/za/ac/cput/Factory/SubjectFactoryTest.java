7dc4c33a2f600d87f0694787973e476144d85e1f
SergioFisher <217203248@mycput.ac.za>
2021-06-10 22:59:21 +0200

    Created java directory

--- src/test/java/za/ac/cput/Factory/SubjectFactoryTest.java
+++ src/test/java/za/ac/cput/Factory/SubjectFactoryTest.java
@@ -1,33 +1,7 @@
 package za.ac.cput.Factory;
 
-import org.junit.jupiter.api.BeforeEach;
-import za.ac.cput.Entity.Subject;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-
-/**
- * Subject.java
- * Entity for Subject
- * Author: Mathew Fortuin (219069514)
- * Date: 6/6/21
- *
- */
-
-class SubjectFactoryTest
-{
-
-    private Subject sub1;
-    private Subject sub2;
-    private Subject sub3;
-
-
-    @BeforeEach
- void setup()
-{
-
-    sub1 = new Subject();
-
-}
+class SubjectFactoryTest {
 
 }
