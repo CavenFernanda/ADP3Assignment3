c6b42f99e02a3129c929ee442dc9d3880db2687b
Project3Assignment <mfortuin509@gmail.com>
2021-06-10 22:04:41 +0200

    created Factory class for my Subject entity

---
+++ src/main/java/za/ac/cput/Factory/SubjectFactory.java
@@ -0,0 +1,22 @@
+package za.ac.cput.Factory;
+
+import za.ac.cput.Entity.Subject;
+/**
+ * Subject.java
+ * Entity for Subject
+ * Author: Mathew Fortuin (219069514)
+ * Date: 10/6/21
+ *
+ */
+
+
+
+public class SubjectFactory
+{
+
+public static Subject build(String subjectName,String subjectCredits,int subjectID,int lecturerID)
+{
+    //
+}
+
+}
